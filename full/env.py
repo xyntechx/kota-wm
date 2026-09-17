baa2c2b29e1b2b7273d81e5d9bdc4ce8b6b7fd8f
xyntechx <xyntechx@gmail.com>
2026-09-16 20:25:29 -0700

    interactive grid

--- full/env.py
+++ full/env.py
@@ -40,21 +40,60 @@ class City:
             self.grid[node[0]][node[1]] = "A"
 
         assert (
-            self.grid[spawn_point[0]][spawn_point[1]] == 0
-        ), "Player must spawn in a building, not on a road."
+            self.grid[spawn_point[0]][spawn_point[1]] == 1
+        ), "Player must spawn on a free road, not in a building or at a traffic light."
 
         # Set player start coordinates / spawn point
-        self.player_row, self.player_col = spawn_point
-        self.grid[self.player_row][self.player_col] = "P"
+        self.p_row, self.p_col = spawn_point
+        self.grid[self.p_row][self.p_col] = "P"
+        self.prev_cell = 1
 
     def step(self, action):
+        """
+        Args:
+        - action: str -- "W" (move North), "A" (move West), "S" (move South), "D" (move East)
+        """
         # TODO: reward (incl task fulfillment), traffic light switch
-        pass
 
+        future_row, future_col = self.p_row, self.p_col
+        match action:
+            case "W":
+                future_row -= 1
+            case "A":
+                future_col -= 1
+            case "S":
+                future_row += 1
+            case "D":
+                future_col += 1
 
-if __name__ == "__main__":
+        rew = 0
+        termination = False
 
-    def draw_color(cell):
+        if not self.graph.has_node((future_row, future_col)):
+            # player attempts to exit road, e.g. go off grid, go into building
+            # severe punishment, terminate episode
+            rew -= 10
+            termination = True
+        elif self.graph.has_edge((self.p_row, self.p_col), (future_row, future_col)):
+            # player follows traffic flow (not driving in the opposite lane)
+            # normal reward, continue episode
+            rew += 1
+            self._move_player(future_row, future_col)
+        else:
+            # player does not follow traffic flow (driving in the opposite lane)
+            # normal punishment, continue episode
+            rew -= 1
+            self._move_player(future_row, future_col)
+
+        return rew, termination
+
+    def _move_player(self, future_row, future_col):
+        self.grid[self.p_row][self.p_col] = self.prev_cell
+        self.prev_cell = self.grid[future_row][future_col]
+        self.p_row, self.p_col = future_row, future_col
+        self.grid[self.p_row][self.p_col] = "P"
+
+    def _color(self, cell):
         if cell == 0:
             return colored(cell, "blue")
         elif cell == 1:
@@ -64,6 +103,17 @@ if __name__ == "__main__":
         elif cell == "A" or cell == "S":
             return colored(cell, "red")
 
-    city = City(spawn_point=(2, 9))
-    for hoz in city.grid:
-        print(" ".join([draw_color(x) for x in hoz]))
+    def __str__(self):
+        out = "\n".join([" ".join([self._color(x) for x in hoz]) for hoz in self.grid])
+        return out
+
+
+if __name__ == "__main__":
+    city = City(spawn_point=(4, 8))
+    termination = False
+
+    while not termination:
+        print(city)
+        action = input("::")
+        rew, termination = city.step(action)
+        print(f"Reward: {rew}")
